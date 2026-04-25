1a984feb54286b3d28dc958e6132d21038d43726
if25b050 <if25b050@technikum-wien.at>
2026-04-25 16:41:08 +0200

    Ping User Function with Redis

--- selfStudy5/app/index.ts
+++ selfStudy5/app/index.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify';
 import { PrismaClient } from '@prisma/client';
+import { createClient } from "redis";
+
 
 const prisma = new PrismaClient();
 
@@ -7,6 +9,14 @@ const server = fastify();
 
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '127.0.0.1';
+
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_HOST = process.env.REDIS_HOST || 'redis';
+
+const redis = createClient({
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+}).on("error", (err) => console.log("Redis Client Error", err));
+
 
 server.get('/ping', async (_, reply) => {
   try {
@@ -16,7 +26,27 @@ server.get('/ping', async (_, reply) => {
 
     const count = await prisma.counter.count();
 
-    return reply.status(200).send({ count });
+    return reply.status(200).send({ count: "TEST "+ count });
+  } catch (error: any) {
+    return reply.status(500).send({ error: error?.message });
+  }
+});
+
+server.get('/ping/:user', async (request, reply) => {
+
+  const user = (request.params as {user:string}).user;
+  try {
+    await redis.connect();
+
+    const value = await redis.get(user);
+    if(value) {
+      redis.quit();
+      return reply.status(200).send({ "message": "Cache hit" });
+    } else {
+      redis.set(user, "true");
+      redis.quit();
+      return reply.status(200).send({ "message": "User added to cache" });
+    }
   } catch (error: any) {
     return reply.status(500).send({ error: error?.message });
   }
